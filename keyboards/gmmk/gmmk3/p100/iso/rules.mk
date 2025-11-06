749a8983bf990b2c1bfc39cea80c8b8c712e48f7
Kirsty Luke <1482576+thecontrarycat@users.noreply.github.com>
2025-11-06 23:14:41 +0000

    Add GMMK3 100% ISO

---
+++ keyboards/gmmk/gmmk3/p100/iso/rules.mk
@@ -0,0 +1,17 @@
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+ENCODER_ENABLE = yes		# Enable rotary encoder
+MOUSEKEY_ENABLE = no       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+AUDIO_ENABLE = no           # Audio output
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = yes           # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+RGB_MATRIX_ENABLE = yes		# Enable keyboard keys RGB
+DIP_SWITCH_ENABLE = yes		# Enable DIP Switch on the back
+# USB_WAIT_FOR_ENUMERATION = yes
+# NO_USB_STARTUP_CHECK = yes
